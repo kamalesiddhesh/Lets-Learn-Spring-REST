cadb17299766fa6844a6b155ba951f9c7a73bdd7
Siddhesh <kamale.siddhesh@gmail.com>
2024-05-11 12:24:24 +0530

    Spring REST Application

---
+++ 27-SB-REST-API/src/main/java/in/ashokit/rest/CustomerRestController.java
@@ -0,0 +1,38 @@
+package in.ashokit.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.ashokit.binding.Customer;
+
+@RestController
+public class CustomerRestController {
+	
+	@GetMapping(
+			value ="/customer",
+			produces = {"application/xml","application/json"}
+	)
+	public Customer getCustomer() {
+		Customer c = new Customer();
+		c.setName("John");
+		c.setEmail("john@gmail.com");
+		c.setGender("Male");
+		return c;
+	}
+	
+	@PostMapping(
+			value = "/customer",
+			consumes = {"application/json","application/xml"},
+			produces = {"text/plain"}
+	)
+	public ResponseEntity<String> addCustomer(@RequestBody Customer customer){
+		System.out.println(customer);
+		// Logic to add customer date to DB.......
+		return new ResponseEntity<>("Customer Saved..!!",HttpStatus.CREATED);
+	}
+
+}
